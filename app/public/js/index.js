c1bfc1111353db68bbcafb5c6f28746069e108d3
krferrar <89742853+krferrar@users.noreply.github.com>
2021-10-29 00:23:54 -0400

    edit/delete try 1

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -36,6 +36,29 @@ const Book1 = {
               console.error(err);
           });
     },
+    postEditBook(evt) {
+      this.bookForm.id = this.selectedBook.id;
+      //this.bookForm.studentId = this.selectedStudent.id;        
+      
+      console.log("Editing!", this.offerBook);
+
+      fetch('api/book/update.php', {
+          method:'POST',
+          body: JSON.stringify(this.bookForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.offers = json;
+          
+          // reset the form
+          this.handleResetEdit();
+        });
+    },
     postNewBook(evt) {
       //this.bookForm.bookId = this.selectedBook.id;        
       
@@ -57,7 +80,40 @@ const Book1 = {
           // reset the form
           this.bookForm = {};
         });
-    }
+    
+},
+postDeleteBook(o) {  
+  if ( !confirm("Are you sure you want to delete the book from " + o.title + "?") ) {
+      return;
+  }  
+  
+  console.log("Delete!", o);
+
+  fetch('api/books/delete.php', {
+      method:'POST',
+      body: JSON.stringify(o),
+      headers: {
+        "Content-Type": "application/json; charset=utf-8"
+      }
+    })
+    .then( response => response.json() )
+    .then( json => {
+      console.log("Returned from post:", json);
+      // TODO: test a result was returned!
+      this.books = json;
+      
+      // reset the form
+      this.handleResetEdit();
+    });
+},
+handleEditBook(book) {
+    this.selectedBook = book;
+    this.bookForm = Object.assign({}, this.selectedBook);
+},
+handleResetEdit() {
+    this.selectedBook= null;
+    this.bookForm = {};
+}
 },
       
     created() {
